56aaed4b96e48ccad71305fe0b1afa5d2425e67c
FahmidaMahjabin <mehjabineva.ce@gmail.com>
2022-05-26 00:19:25 +0600

    avatar of user and logOut

--- src/Components/HomePage/Navbar/Navbar.js
+++ src/Components/HomePage/Navbar/Navbar.js
@@ -1,8 +1,12 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { Link, NavLink } from 'react-router-dom';
+import auth from '../../../init';
 
 const Navbar = () => {
-    
+    const [user] = useAuthState(auth);
+    console.log("user:", user)
     return (
         <div className="navbar bg-base-100 bg-primary">
             <div className="navbar-start">
@@ -11,46 +15,74 @@ const Navbar = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabindex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><Link to ="/" >Home</Link></li>
-                        <li><Link to ="/purchase" >Purchase</Link></li>
+                        <li><Link to="/" >Home</Link></li>
+                        <li><Link to="/purchase" >Purchase</Link></li>
+                        {user ?
+                            <li tabindex="0">
+                                <Link to="/dashBoard" className="justify-between">
+                                    DashBoard
+                                    <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
+                                </Link>
+                                <ul className="p-2">
+                                    <li><Link to="/myOrders" >My Orders</Link></li>
+                                    <li><Link to="/addAReview" >Add A Review</Link></li>
+                                    <li><Link to="/myProfile" >My Profile</Link></li>
+                                </ul>
+                            </li>
+                            :
+                            <div className="navbar-end">
+                                <Link to="/logOut" className="btn">Log In</Link>
+                            </div>
+                        }
+
+
+                    </ul>
+                </div>
+                <Link to="/" className="btn btn-ghost normal-case text-xl">Computer Parts Manufactur </Link>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal p-0">
+                    <li><Link to="/" >Home</Link></li>
+                    <li><Link to="/purchase">Purchase</Link></li>
+
+                    {user ?
                         <li tabindex="0">
-                            <Link to = "/dashBoard" className="justify-between">
+                            <Link to="/dashBoard" className="justify-between">
                                 DashBoard
                                 <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
                             </Link>
                             <ul className="p-2">
-                                <li><Link to = "/myOrders" >My Orders</Link></li>
-                                <li><Link to ="/addAReview" >Add A Review</Link></li>
-                                <li><Link to ="/myProfile" >My Profile</Link></li>
+                                <li><Link to="/myOrders" >My Orders</Link></li>
+                                <li><Link to="/addAReview" >Add A Review</Link></li>
+                                <li><Link to="/myProfile" >My Profile</Link></li>
                             </ul>
                         </li>
-                        
-                    </ul>
-                </div>
-                <Link to ="/"  className="btn btn-ghost normal-case text-xl">Computer Parts Manufactur </Link>
-            </div>
-            <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal p-0">
-                    <li><Link to ="/" >Home</Link></li>
-                    <li><Link to = "/purchase">Purchase</Link></li>
+                        :
+                        <></>
+                    }
 
-                    <li tabindex="0">
-                        <Link to = "/dashBoard" >
-                            DashBoard
-                            <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
-                        </Link>
-                        <ul className="p-2">
-                            <li><Link to = "/dashBoard/myOrders" >My Orders</Link></li>
-                            <li><Link to = "/dashBoard/addAReview" >Add A Review</Link></li>
-                            <li><Link to = "/dashBoard/myProfile" >My Profile</Link></li>
-                        </ul>
-                    </li>
-                    
                 </ul>
             </div>
-            <div className="navbar-end">
-                <Link to = "/logOut"  className="btn">Log Out</Link>
-            </div>
+            {
+                user ?
+                    <div className="navbar-end">
+                        {/* avatar */}
+                        <div class="avatar">
+                            <div class="w-16 rounded-full">
+                                <img src={user?.photoURL} />
+                            </div>
+                        </div>
+                        <div >
+                            <button onClick={() => signOut(auth)} className="btn">Log Out</button>
+                        </div>
+                    </div>
+                    :
+                    <div >
+                        <NavLink to = "/logIn" onClick={() => signOut(auth)} className="btn">Log In</NavLink>
+                    </div>
+
+            }
+
         </div>
     );
 };
