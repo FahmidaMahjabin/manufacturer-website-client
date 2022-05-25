767c17d4d8de72e751e075446c9e88dc8e722b2a
FahmidaMahjabin <mehjabineva.ce@gmail.com>
2022-05-25 21:37:49 +0600

    user login validation

--- src/Components/LogIn/LogIn.js
+++ src/Components/LogIn/LogIn.js
@@ -4,24 +4,24 @@ import { useSignInWithEmailAndPassword, useSignInWithGoogle } from "react-fireba
 import auth from '../../init';
 import { useForm } from "react-hook-form";
 const LogIn = () => {
-    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const [signInWithGoogle, guser, gloading, gerror] = useSignInWithGoogle(auth);
     const { register, formState: { errors }, handleSubmit } = useForm();
 
     // login using email password
     // step1:get email and password from email and password field when blur and set it to start
     // pass that email, password to the signInWithEmailPassword function when click to submite
-    // const [email, setEmail] = useState("");
-    // const [password, setPassword] = useState("");
-    // const [
-    //     signInWithEmailAndPassword
-    // ] = useSignInWithEmailAndPassword(auth);
-    if (user) {
-        console.log("user:", user)
-    }
-    
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+
+
     // onsubmit for login
     const onSubmit = (data) => {
-        console.log("data:", data)
+        console.log("data:", data);
+        signInWithEmailAndPassword(data.email, data.password)
 
     }
     return (
@@ -34,7 +34,7 @@ const LogIn = () => {
                 <div className="card  w-full max-w-sm shadow-2xl bg-base-100">
                     <div className="card-body">
                         {/* form hook starts */}
-                        
+
                         {/* form hook ends */}
                         <form onSubmit={handleSubmit(onSubmit)}>
                             <div className="form-control">
@@ -42,16 +42,16 @@ const LogIn = () => {
                                     <span className="label-text">Email</span>
                                 </label>
                                 <input type="email" placeholder="email" className="input input-bordered"
-                                {...register("email", {
-                                    required: {
-                                        value: true,
-                                        message: "email is required"
-                                    },
-                                    pattern: {
-                                        value: /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/,
-                                        message: 'email is not correct' // JS only: <p>error message</p> TS only support string
-                                    }
-                                })} />
+                                    {...register("email", {
+                                        required: {
+                                            value: true,
+                                            message: "email is required"
+                                        },
+                                        pattern: {
+                                            value: /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/,
+                                            message: 'email is not correct' // JS only: <p>error message</p> TS only support string
+                                        }
+                                    })} />
 
                                 {/* error message */}
                                 {errors.email?.type === 'required' && <span className='text-red-700'>email  is required</span>}
@@ -63,16 +63,16 @@ const LogIn = () => {
                                     <span className="label-text" >Password</span>
                                 </label>
                                 <input type="password" placeholder="password" className="input input-bordered"
-                                {...register("password", {
-                                    required: {
-                                        value: true,
-                                        message: "password is required"
-                                    },
-                                    minLength: {
-                                        value: 6,
-                                        message: 'character should be more than 6' // JS only: <p>error message</p> TS only support string
-                                    }
-                                })} />
+                                    {...register("password", {
+                                        required: {
+                                            value: true,
+                                            message: "password is required"
+                                        },
+                                        minLength: {
+                                            value: 6,
+                                            message: 'character should be more than 6' // JS only: <p>error message</p> TS only support string
+                                        }
+                                    })} />
                                 {/* error message of password */}
                                 {errors.password?.type === 'required' && <span className='text-red-700'>password is needed</span>}
                                 {errors.password?.type === 'minLength' && <span className='text-red-700'>character should be more than 6</span>}
@@ -83,10 +83,15 @@ const LogIn = () => {
                             <div className="form-control mt-6">
                                 <button type="submit" className="btn btn-primary" >Login</button>
                             </div>
+
+                            {/* error for login */}
+                            {error || gerror?
+                                <p className='text-red-500'>{error?.message|| gerror.message}</p>: ""
+                            }
                         </form>
                         {/* divider */}
                         <div className='divider'>OR</div>
-                        <div className='btn btn-outline btn-primary' onClick={() => signInWithGoogle() } >LogIn with Google</div>
+                        <div className='btn btn-outline btn-primary' onClick={() => signInWithGoogle()} >LogIn with Google</div>
                     </div>
                 </div>
             </div>
