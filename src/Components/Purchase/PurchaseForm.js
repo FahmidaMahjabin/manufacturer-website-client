cf7d16e2117390de235c32f2773b2618322a6ebe
FahmidaMahjabin <mehjabineva.ce@gmail.com>
2022-05-26 21:42:24 +0600

    purchase done

--- src/Components/Purchase/PurchaseForm.js
+++ src/Components/Purchase/PurchaseForm.js
@@ -1,26 +1,52 @@
+import axios from 'axios';
 import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useForm } from 'react-hook-form';
 import auth from '../../init';
 
 const PurchaseForm = ({item}) => {
-    const { register, handleSubmit,  formState: { errors } } = useForm();
+    const [user] = useAuthState(auth); 
+    const {_id, name, quantity, minimumQuantity} = item;
+    const { register,handleSubmit, formState: { errors } } = useForm();
     // function = onSubmit (click korle quantity kombe and purchase list e add hobe)
     // step1:oi object er id ta nobo
     // step2:oi id diye query kore item er quantity ager quantity theke new quantity ta minus korbo
     // step3:new ekta collection purchase e name diye query korbo j ase kina 
         // step3.1:jodi item thake tahole quantity increase korbo 
         // step4: nam thakle item insert korbo
-
-    const onSubmit = (data) => {
-        console.log("data from purchase submit:", data);
-    }
-    const [user] = useAuthState(auth);
     
-    const {quantity, minimumQuantity} = item;
+    const purchaseItem = (name, id, e) =>{
+        console.log("e:", e)
+        e.preventDefault();
+        const purchedQuantity = e.target.quantity.value;
+        
+        const purchesdItem = {
+            purchedId: id,
+            purchedQuantity: purchedQuantity,
+            product: name,
+            
+        }
+        console.log("purchedItem:", purchesdItem)
+        fetch("http://localhost:5000/purchase", {
+            method: 'PUT',
+            headers:{
+                'content-type': "application/json"
+            },
+            body: JSON.stringify(purchesdItem)
+        })
+        .then(res => res.json())
+        .then(data => console.log("data from purchaseItem:", data))
+
+    }
+    
+    
+    
+   
+
+    
     
     return (
-        <form onSubmit={handleSubmit(onSubmit)} className = "w-1/2 mx-auto bg-gray-300 p-8">
+        <form onSubmit={(e) =>purchaseItem(name, _id, e)}  className = "w-1/2 mx-auto bg-gray-300 p-8">
             <div class="form-control">
                 <label class="label">
                     <span class="label-text font-bold">Name</span>
@@ -51,7 +77,7 @@ const PurchaseForm = ({item}) => {
                 <label class="label">
                     <span class="label-text font-bold">Purchase Quantity</span>
                 </label>
-                <input type="text" placeholder="Enter quantity" class="input input-bordered"
+                <input type="text" placeholder="Enter quantity" name = "quantity" class="input input-bordered"
                 {...register("quantity", { required: true 
                 ,
                 min: {
