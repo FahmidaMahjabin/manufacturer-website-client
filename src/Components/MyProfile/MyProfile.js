d39d630b45183fb148ba54f4f82512187fcaa6c7
FahmidaMahjabin <mehjabineva.ce@gmail.com>
2022-05-27 23:19:36 +0600

    myProfile page add

--- src/Components/MyProfile/MyProfile.js
+++ src/Components/MyProfile/MyProfile.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../init';
+import ProfileForm from './ProfileForm';
 
 const MyProfile = () => {
+    const [user] = useAuthState(auth);
+    const {displayName, photoURL, email} = user;
+    const [myProfile, setMyProfile] = useState({});
+    useEffect(()=>{
+        fetch('http://localhost:5000/myProfile')
+        .then(res => res.json())
+        .then(data => setMyProfile(data[0]))
+    },
+    [])
+    console.log("myProfile:",myProfile )
     return (
         <div>
-            <h1>My Profile</h1>
+            <h1 className='text-5xl text-center my-8'>My Profile</h1>
+            <div class="card card-side bg-base-100 shadow-xl bg-gray-100">
+                <figure><img src={photoURL} alt="propic"/></figure>
+                <div class="card-body">
+                    <h2 class="card-title">{displayName}</h2>
+                    <div>
+                        <span className='font-bold'>Email:</span>{email}
+                        {(myProfile.location)?(<p><span className='font-bold'>Location:</span>{myProfile?.location}</p> ) : ""}
+                        {myProfile.education?(<p><span className='font-bold'>Education:</span>{myProfile?.education}</p> ) : ""}
+                        {myProfile.phoneNo?(<p><span className='font-bold'>ContactNo:</span>{myProfile?.phoneNo}</p> ) : ""}
+                        {myProfile.socialProfile?(<p><span className='font-bold'>Social Profile:</span>{myProfile?.socialProfile}</p>)  : ""}
+
+                    </div>
+                    
+                </div>
+            </div>
+            <ProfileForm></ProfileForm>
         </div>
     );
 };
---
+++ src/Components/MyProfile/ProfileForm.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
+
+const ProfileForm = () => {
+    const { register, handleSubmit } = useForm();
+   
+    const onSubmit = data =>{
+        console.log(data);
+        fetch('http://localhost:5000/myProfile', {
+            method: 'PUT',
+            headers:{
+                'content-type': "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+        .then(res => res.json())
+        .then(result => console.log(result))
+
+    } 
+    return (
+        <form onSubmit={handleSubmit(onSubmit)} className='mx-auto my-5 bg-red-200 p-4 rounded-sm w-1/2 '>
+            <div className='form-control'>
+                <label for="education" className='font-bold'>Eduaction:</label>
+                <input name="education" {...register("education")} type="text" placeholder="Institution" class="input input-bordered input-primary w-full max-w-xs " />
+            </div>
+
+            <div className='form-control'>
+                <label for="location" className='font-bold'>location:</label>
+                <input name="location" {...register("location")} type="text" placeholder="location" class="input input-bordered input-primary w-full max-w-xs" />
+            </div>
+
+            <div className='form-control'>
+                <label for="phoneNo" className='font-bold'>phoneNo:</label>
+                <input name="phoneNo" {...register("phoneNo")} type="number" placeholder="phoneNo" class="input input-bordered input-primary w-full max-w-xs" />
+            </div>
+
+            <div className='form-control'>
+                <label for="socialProfile" className='font-bold'>socialProfile:</label>
+                <input name="socialProfile" {...register("socialProfile")} type="text" placeholder="linkedIn/facebook" class="input input-bordered input-primary w-full max-w-xs" />
+            </div>
+            <button className='btn btn-primary btn-block my-6 w-1/2'>Submit</button>
+        </form>
+    );
+};
+
+export default ProfileForm;
